d7abbd845908237e6413e785a426e64f8749e0eb
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-10-01 08:48:44 +0300

    ADD UI kit basic template

---
+++ template-parts/ui/colors.php
@@ -0,0 +1,265 @@
+<div class="row ui__colors--bg">
+    <div class="col-md-3 mb-3">
+        <div class="p-2 bg--black">
+            <h4 class="text-color-white">Color Black</h4>
+            <p class="text-color-white mt-1">.bg--black</p>
+        </div>
+    </div>
+    <div class="col-md-3 mb-3">
+        <div class="p-2 bg--primary">
+            <h4 class="text-color-white">Primary</h4>
+            <p class="text-color-white mt-1">.bg--primary</p>
+        </div>
+    </div>
+    <div class="col-md-3 mb-3">
+        <div class="p-2 bg--secondary">
+            <h4 class="text-color-white">Bg secondary</h4>
+            <p class="text-color-white mt-1">.bg--secondary</p>
+        </div>
+    </div>
+    <div class="col-md-3 mb-3">
+        <div class="p-2 bg--gradient-orange">
+            <h4 class="">Bg gradient orange</h4>
+            <p class=" mt-1">.bg--gradient-orange</p>
+        </div>
+    </div>
+</div>
+<div class="mt-3">
+    <p class="text-color-primary pb-1">.text-color-primary</p>
+    <p class="text-color-default pb-1">.text-color-default</p>
+    <p class="text-color-secondary pb-1">.text-color-secondary</p>
+    <p class="text-color-white bg--dark pb-1">.text-color-white</p>
+</div>
+
+MENU 
+
+font-family: Avenir;
+font-size: 17px;
+font-weight: 500;
+line-height: 19px;
+letter-spacing: 0em;
+text-align: left;
+
+
+HOMEPAGE BANNER
+
+“Medicine Is Complicated. Billing Shouldn’t Be.” 
+
+font-family: Avenir;
+font-size: 113px;
+font-weight: 500;
+line-height: 127px;
+letter-spacing: 0em;
+text-align: left;
+
+Customized Revenue & Growth Solutions
+
+font-family: Avenir;
+font-size: 42px;
+font-weight: 500;
+line-height: 47px;
+letter-spacing: 0em;
+text-align: left;
+
+HOMEPAGE SECTION 2
+
+Why Leading Organizations Partner with Us   
+
+font-family: Avenir;
+font-size: 60px;
+font-weight: 500;
+line-height: 67px;
+letter-spacing: 0em;
+text-align: left;
+
+
+
+Fellow Health Partners is committed to helping our clients improve their bottom line and grow their business by applying innovative medical business solutions.
+
+font-family: Avenir;
+font-size: 32px;
+font-weight: 500;
+line-height: 36px;
+letter-spacing: 0em;
+text-align: left;
+
+
+We meet this commitment by fusing Technology, Talent, and Training to create a multi-faceted support system to meet our clients’ needs.
+
+Founded by business professionals and clinicians, our end-to-end revenue cycle solutions integrate with our customers’ existing infrastructures to seamlessly streamline billing, collections, coding, unresolved denials, and related workflows.
+
+
+font-family: Avenir;
+font-size: 19px;
+font-weight: 500;
+line-height: 27px; / 24PX
+letter-spacing: 0em;
+text-align: left;
+
+font-family: Articulat CF;
+font-size: 19px;
+font-weight: 500;
+line-height: 27px;
+letter-spacing: 0em;
+text-align: left;
+
+WHAT WE DO
+
+What We Do 
+
+font-family: Avenir;
+font-size: 80px;
+font-weight: 500;
+line-height: 90px;
+letter-spacing: 0em;
+text-align: right;
+
+For Hospital Systems, ASCs, and Physician Groups, the heart of the financial business operations is Revenue Cycle
+
+font-family: Avenir;
+font-size: 21px;
+font-weight: 500;
+line-height: 29px;
+letter-spacing: 0em;
+text-align: left;
+
+Revenue Cycle Management Process
+
+font-family: Avenir;
+font-size: 50px;
+font-weight: 500;
+line-height: 56px;
+letter-spacing: 0em;
+text-align: left;
+
+
+font-family: Avenir;
+font-size: 32px;
+font-weight: 500;
+line-height: 36px;
+letter-spacing: 0em;
+text-align: right;
+
+
+20% 
+
+font-family: Avenir;
+font-size: 100px;
+font-weight: 900;
+line-height: 122px;
+letter-spacing: 0em;
+text-align: left;
+
+
+reduction in cost to collect
+
+font-family: Avenir;
+font-size: 24px;
+font-weight: 500;
+line-height: 36px;
+letter-spacing: 0em;
+text-align: left;
+
+This includes growing revenue, billings, patient visits, procedures, and surgeries. We can also help you streamline operations, reduce costs, and identify both opportunities and potential pitfalls. 
+
+ADVISORY SERVICES
+
+font-family: Avenir;
+font-size: 18px;
+font-weight: 500;
+line-height: 25px; / 22PX - FORM PLACEHOLDER
+letter-spacing: 0em;
+text-align: left;
+
+20%
+
+font-family: Avenir;
+font-size: 70px;
+font-weight: 900;
+line-height: 85px;
+letter-spacing: 0em;
+text-align: left;
+
+
+Improve efficiency, find new revenue, and build a more sustainable revenue cycle.
+
+font-family: Avenir;
+font-size: 32px;
+font-weight: 500;
+line-height: 45px;
+letter-spacing: 0em;
+text-align: left;
+
+    TEAM
+
+    bio
+
+    font-family: Avenir;
+font-size: 27px;
+font-weight: 900;
+line-height: 30px;
+letter-spacing: 0em;
+text-align: left;
+
+Read more testimonials
+
+font-family: Avenir;
+font-size: 21px;
+font-weight: 500;
+line-height: 24px;
+letter-spacing: 0em;
+text-align: left;
+
+
+CPC TRAINING PAGE
+
+$2395.00
+
+font-family: Avenir;
+font-size: 32px;
+font-weight: 900;
+line-height: 39px;
+letter-spacing: 0em;
+text-align: left;
+
+
+
+COMPLIENCE
+
+Why Choose Fellow Health Partners for Your Medical Billing Needs?
+
+font-family: Avenir;
+font-size: 49px;
+font-weight: 500;
+line-height: 55px;
+letter-spacing: 0em;
+text-align: left;
+
+When it comes to medical billing, compliance is not just a checkbox—it's a fundamental principle that protects your practice, patients, and reputation.
+
+font-family: Avenir;
+font-size: 26px;
+font-weight: 500;
+line-height: 29px;
+letter-spacing: 0em;
+text-align: left;
+
+
+FOOTER MENU TOP
+
+font-family: Avenir;
+font-size: 26px;
+font-weight: 500;
+line-height: 24px;
+letter-spacing: 0em;
+text-align: left;
+
+
+FORM LABEL
+
+font-family: Avenir;
+font-size: 16px;
+font-weight: 500;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: center;
