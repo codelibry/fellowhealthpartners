6329743ebd7a798ee97d7f210cced3ba1d9edb0d
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-10-02 10:40:00 +0300

    Add buttons UI kit

--- template-parts/ui/buttons.php
+++ template-parts/ui/buttons.php
@@ -1,7 +1,32 @@
-<div class="ui__buttons">
-    <h3 class="pt-3 pb-2">.button</h3>
-    <a href="#" class="button m-1">Button</a>
-
-    <h3 class="pt-3 pb-2">.link-underline</h3>
-    <a href="#" class="link-underline m-1">Text link</a>
+<div class="ui__buttons row">
+    <div class="col-md-3 mb-3">
+        <h3 class="pt-3 pb-2">.button.button--sm</h3>
+        <a href="#" class="button button--sm bg--gradient-orange text-color-white">Button</a>
+    </div>
+    <div class="col-md-3 mb-3">
+        <h3 class="pt-3 pb-2">.button</h3>
+        <a href="#" class="button bg--gradient-orange text-color-white">Button</a>
+    </div>
+    <div class="col-md-6 mb-3">
+        <h3 class="pt-3 pb-2">.button.button--fluid</h3>
+        <a href="#" class="button button--fluid bg--gradient-orange text-color-white">Button</a>
+    </div>
+    <div class="col-12 mb-3">
+        <h3 class="pt-3 pb-2">.button.button--xl</h3>
+        <a href="#" class="button button--fluid button--lg bg--secondary text-color-white">Button</a>
+    </div>
+    <div class="col-md-4 mb-3">
+        <h3 class="pt-3 pb-2">.button.button--outline</h3>
+        <a href="#" class="button button--outline text-color-primary">Button outline</a>
+    </div>
+    <div class="col-md-4 mb-3">
+        <h3 class="pt-3 pb-2">.button.button--sm.button--lg</h3>
+        <a href="#" class="button button--lg button--sm bg--gradient-orange text-color-white">SAVi Client Portal</a>
+    </div>
+    <div class="col-md-3 mb-3">
+        <h3 class="pt-3 pb-2">.link-underline</h3>
+        <a href="#" class="button text-color-primary d-flex align-items-center p-0">Text link <?php echo get_inline_svg('arrows/arrow-right.svg');?></a>
+    </div>
 </div>
+
+    
