4446084d48beb27cec3a31b4a98ef31a7faacbba
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-10-02 11:49:26 +0300

    Add forms styles

--- template-parts/ui/form.php
+++ template-parts/ui/form.php
@@ -1,3 +1,18 @@
-<div class="form">
-    <?php echo do_shortcode('[ninja_form id=2]'); ?>
+<div class="form row">
+    <div class="col-md-4">
+        <label>Your Name</label>
+        <input type="text" placeholder="Your Name">
+    </div>
+    <div class="col-md-4">
+        <label>Your Name</label>
+        <input type="email" placeholder="Your Email">
+    </div>
+    <div class="col-md-4">
+        <label>Comment</label>
+        <textarea placeholder="Your Comment"></textarea>
+    </div>
+    <div class="col-md-4">
+        <input type="submit" placeholder="Submit">
+    </div>
+    
 </div>
