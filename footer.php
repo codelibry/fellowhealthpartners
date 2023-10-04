31296ebae6d364c2a11156d4b6ab26958dfe1078
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-10-04 11:55:39 +0300

    Careers + Careers single code review

--- footer.php
+++ footer.php
@@ -19,49 +19,6 @@
 
 <?php wp_footer(); ?>
 
-
-<svg class="clip-svg visually-hidden">
-	<defs>
-		<clipPath id="waveTop" clipPathUnits="objectBoundingBox">
-			<path style="fill:#F8F8F8;" d="M-0.0203,1.1149l1.0409-0.0032V0.0385c0,0-0.32-0.0588-0.5529-0.0166
-	c-0.2328,0.0421-0.488,0.019-0.488,0.019V1.1149z" />
-		</clipPath>
-		<clipPath id="waveTopMobile" clipPathUnits="objectBoundingBox">
-			<path style="fill:#964040;" d="M-0.0097,0.009c0,0-0.0547-0.0047,0.2031-0.0047c0.2359,0,0.2828,0.0078,0.5609,0.0141 C1.0027,0.024,1.0028,0.0137,1.0387,0.0137L1.0155,10.1085h-1.0252V0.009z" />
-		</clipPath>
-		<clipPath id="waveBottom" clipPathUnits="objectBoundingBox">
-			<path style="fill:#964040;" d="M-0.0113,0.9557c0,0,0.1344,0.0344,0.2766,0.0391c0.1422,0.0047,0.2428-0.0133,0.2984-0.0203 c0.1234-0.0156,0.1734-0.0281,0.2688-0.025c0.1033,0.0034,0.1703,0.0172,0.1703,0.0172v-1.4453l-1.0094-0.0016L-0.0113,0.9557z" />
-		</clipPath>
-
-		<clipPath id="waveImageBottom" clipPathUnits="objectBoundingBox">
-			<path style="fill:#964040;" d="M-0.0006,0.8942c0,0,0.1037,0.0989,0.2537,0.1018c0.2453,0.0047,0.2438-0.0703,0.4313-0.0891 c0.1156-0.0116,0.3135-0.0065,0.3135-0.0065v-1.5364H0L-0.0006,0.8942z" />
-		</clipPath>
-
-	</defs>
-</svg>
-
-<style>
-	@media only screen and (min-width: 992px) {
-		#custom_html-2 .gallery.gallery-columns-4 .gallery-item {
-			width: 20%;
-		}
-	}
-
-	@media only screen and (min-width: 1200px) {
-		body footer.site-footer .site-footer__col-right .footer-logos-1 .textwidget {
-			max-width: 40rem;
-		}
-	}
-
-	@media only screen and (max-width: 992px) {
-		footer.site-footer .site-footer__col-right .widget .gallery {
-			-webkit-box-pack: center;
-			-ms-flex-pack: center;
-			justify-content: center;
-		}
-	}
-</style>
-
 </body>
 
 </html>
--- single-job.php
+++ single-job.php
@@ -20,7 +20,7 @@ if ($the_title == 'Revenue Cycle Associate' || $the_title == 'Account Support As
     <?php get_template_part('template-parts/breadcrumbs/breadcrumbs'); ?>
     <div class="container">
         <div class="back_block section section--spacing--md">
-            <a href="/careers/" class="back-to-category text-color-orange font--weight--500">Back to Careers <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <a href="/careers/" class="back-to-category text-color-orange font--weight--500"><?php _e('Back to Careers','fhp');?> <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M5.08349 9.08398L1.3335 5.33398M1.3335 5.33398L5.08349 1.58398M1.3335 5.33398H11.3335C13.1744 5.33398 14.6668 6.82637 14.6668 8.66731V8.66731C14.6668 10.5083 13.1744 12.0006 11.3335 12.0006H9.66682" stroke="#FF851F" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </a>
@@ -34,11 +34,11 @@ if ($the_title == 'Revenue Cycle Associate' || $the_title == 'Account Support As
                             <h1 class="h3 font--weight--500 text-color-black"><?php the_title(); ?></h1>
                         </div>
                         <div class="job-apply">
-                            <a href="<?php echo '/application-for-employment/?job_title=' . encodeURI($the_title) . '&salaryType=' . $salary . '&emp=' . $emp; ?>" class="button bg--gradient-orange text-color-white font--weight--500" target="_blank">Apply Now</a>
+                            <a href="<?php echo '/application-for-employment/?job_title=' . encodeURI($the_title) . '&salaryType=' . $salary . '&emp=' . $emp; ?>" class="button bg--gradient-orange text-color-white font--weight--500" target="_blank"><?php _e('Apply Now','fhp');?></a>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <div class="content-block text--color-gray">
+                        <div class="content-block text-color-gray">
                             <?php echo the_content(); ?>
                         </div>
                     </div>
@@ -48,9 +48,9 @@ if ($the_title == 'Revenue Cycle Associate' || $the_title == 'Account Support As
                 <div class="job-col col-12 col-lg-6">
                     <?php if (get_field('job_primary_responsibilities')) : ?>
                         <section>
-                            <h3 class="h4 text--color--black_new">Primary Responsibilities</h3>
+                            <h3 class="h4 text-color--black_new"><?php _e('Primary Responsibilities','fhp');?></h3>
                             <div class="line"></div>
-                            <div class="content-block text--color-gray text--size--26">
+                            <div class="content-block text-color-gray text--size--26">
                                 <?php echo get_field('job_primary_responsibilities'); ?>
                             </div>
                         </section>
@@ -58,9 +58,9 @@ if ($the_title == 'Revenue Cycle Associate' || $the_title == 'Account Support As
 
                     <?php if (get_field('job_educationexperience')) : ?>
                         <section class="pt-2 pt-lg-10">
-                            <h3 class="h4 text--color--black_new">Education/Experience</h3>
+                            <h3 class="h4 text-color--black_new"><?php _e('Education/Experience','fhp');?></h3>
                             <div class="line"></div>
-                            <div class="content-block text--color-gray text--size--26">
+                            <div class="content-block text-color-gray text--size--26">
                                 <?php echo  get_field('job_educationexperience'); ?>
                             </div>
                         </section>
@@ -71,9 +71,9 @@ if ($the_title == 'Revenue Cycle Associate' || $the_title == 'Account Support As
                 <div class="job-col col-12 col-lg-6">
                     <?php if (get_field('job_skillsqualifications')) : ?>
                         <section>
-                            <h3 class="h4 text--color--black_new">Skills/Qualifications</h3>
+                            <h3 class="h4 text-color--black_new"><?php _e('Skills/Qualifications','fhp');?></h3>
                             <div class="line"></div>
-                            <div class="content-block text--color-gray text--size--26">
+                            <div class="content-block text-color-gray text--size--26">
                                 <?php echo get_field('job_skillsqualifications'); ?>
                             </div>
                         </section>
