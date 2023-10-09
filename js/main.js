ae06cacb4f0b32c67604f84d105a06a59d091597
Maksym Koitsan <maksym.k@flooow.agency>
2023-10-09 18:19:34 +0300

    add numbers block

--- js/main.js
+++ js/main.js
@@ -30,3 +30,29 @@ videoBlock();
 show_more();
 sliders();
 initPopups();
+$(document).ready(function () {
+  // Add the "active" class to the first block initially
+  $(".number_blocks__main .vertical_block:first").addClass("active");
+
+  $(".number_blocks__main .vertical_block").on("click", function () {
+    // Знайдіть активний блок і заберіть клас "active"
+    var activeBlock = $(".number_blocks__main .vertical_block.active");
+    activeBlock.removeClass("active");
+
+    // Знайдіть блок, який було клікнуто і додайте до нього клас "active"
+    $(this).addClass("active");
+
+    // Перемикаємо відображення блоків у поточному та активному блоках
+    $(".number", activeBlock).show();
+    $(".title", activeBlock).show();
+    $(".bg--white", activeBlock).show();
+    $(".content", activeBlock).hide();
+    $(".title_active", activeBlock).hide();
+
+    $(".number", this).hide();
+    $(".title", this).hide();
+    $(".bg--white", this).hide();
+    $(".content", this).show();
+    $(".title_active", this).show();
+  });
+});
---
+++ template-parts/acf-blocks/number_blocks.php
@@ -0,0 +1,44 @@
+<?php
+$title = get_sub_field('title');
+$numbers = get_sub_field('number_blocks');
+?>
+
+<div class="number_blocks mb-75">
+    <div class="container">
+        <?php if ($title) : ?>
+            <div class="number_blocks__top mb-80">
+                <div class="row">
+                    <div class="col-12 col-lg-7">
+                        <div class="title_block">
+                            <h2><?php echo $title; ?></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+        <?php if ($numbers) : ?>
+            <div class="number_blocks__main">
+                <?php $i = 1;
+                foreach ($numbers as $number) :
+                    $vertical_title = $number['subtitle'];
+                    $content = $number['content'] ?>
+                    <div class="vertical_block d-flex flex-column">
+                        <div class="number h3 text-color-primary">
+                            <?php echo $i ?>.
+                        </div>
+                        <div class="title">
+                            <h3 class="text--size--21 font--weight--800"><?php echo $vertical_title; ?></h3>
+                        </div>
+                        <div class="title_active bg--gradient-orange content-block">
+                            <p class="text-color-white h5 d-flex"><span><?php echo $i ?>.</span> <?php echo $vertical_title; ?></p>
+                        </div>
+                        <div class="content text-color-gray content-block">
+                            <?php echo $content; ?>
+                        </div>
+                    </div>
+                <?php $i++;
+                endforeach; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
