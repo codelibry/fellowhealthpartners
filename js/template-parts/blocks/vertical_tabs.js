3fbb4c6c26fd683f0efef00bd556d81297dbbe91
Maksym Koitsan <maksym.k@flooow.agency>
2023-10-30 18:58:39 +0200

    add hover to tabs

--- js/template-parts/blocks/vertical_tabs.js
+++ js/template-parts/blocks/vertical_tabs.js
@@ -8,37 +8,38 @@ function vertical_Tabs() {
 
   // Toggle Class
   const init = () => {
-    // Menu Click
+    // Menu Click or Hover
     tabLinks.forEach((link) => {
-      link.addEventListener("click", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        // Clear Timers
-        window.clearTimeout(timerOpacity);
-
-        // Toggle Class Logic
-        // Remove Active Classes
-        tabLinks.forEach((link) => link.classList.remove("active"));
-        tabBodies.forEach((body) => {
-          body.classList.remove("active");
-          body.classList.remove("active-content");
-        });
-
-        // Add Active Classes
-        this.classList.add("active");
-        document
-          .querySelector(this.getAttribute("href"))
-          .classList.add("active");
-
-        // Opacity Transition Class
-        timerOpacity = setTimeout(() => {
-          document
-            .querySelector(this.getAttribute("href"))
-            .classList.add("active-content");
-        }, 50);
-      });
+      link.addEventListener("click", toggleTab);
+      link.addEventListener("mouseover", toggleTab);
     });
+  };
+
+  const toggleTab = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Clear Timers
+    window.clearTimeout(timerOpacity);
+
+    // Toggle Class Logic
+    // Remove Active Classes
+    tabLinks.forEach((link) => link.classList.remove("active"));
+    tabBodies.forEach((body) => {
+      body.classList.remove("active");
+      body.classList.remove("active-content");
+    });
+
+    // Add Active Classes
+    this.classList.add("active");
+    document.querySelector(this.getAttribute("href")).classList.add("active");
+
+    // Opacity Transition Class
+    timerOpacity = setTimeout(() => {
+      document
+        .querySelector(this.getAttribute("href"))
+        .classList.add("active-content");
+    }, 50);
   };
 
   // Document Ready
