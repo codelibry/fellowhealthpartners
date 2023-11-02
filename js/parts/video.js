89a9a3fc74df609763a72d4c12cfba5000d339c2
Maksym Koitsan <maksym.k@flooow.agency>
2023-11-02 14:41:32 +0200

    fix poster image

--- js/parts/video.js
+++ js/parts/video.js
@@ -3,15 +3,21 @@ import Plyr from "plyr";
 
 function videoBlock() {
   $(".video-block").each(function () {
+    const acfPosterImage = customjs_ajax_object.getAcfPosterImage;
+
     const player = new Plyr(".player-main", {
       hideControls: false,
       captions: { active: true },
     });
+
     player.toggleControls(false);
 
     player.on("play", (event) => {
       player.toggleControls(true);
     });
+    setTimeout(() => {
+      player.poster = acfPosterImage;
+    }, 100);
   });
 }
 
