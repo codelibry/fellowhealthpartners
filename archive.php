16f93bd220ea834334aafb1f8c086a706d640793
Maksym Koitsan <maksym.k@flooow.agency>
2023-11-02 13:38:12 +0200

    fix tag template

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -8,66 +9,48 @@
  */
 
 get_header();
+
+$linkedin_url = get_field('linkedin_url', 'options');
+$footer_email = get_field('footer_email', 'options');
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+    <?php get_template_part('template-parts/breadcrumbs/breadcrumbs'); ?>
+    <div class="news_page">
         <div class="container">
-            <div class="d-lg-flex section-paddings has-sidebar">
+            <div class="has-sidebar mt-80">
                 <div class="main-content">
-                  
-                        <?php
-                        if ( have_posts() ) :
-
-              
-								?>  
-								<header class="blog-header">
-								<div class="fellow-heading"><h1 class="title"><?php the_archive_title() ?></h1></div>
-								<ul class="socials">
-									<li><a href="<?php echo $fellow_options['linkedin'];?>"><img src="<?php echo get_template_directory_uri(); ?>/images/linkedin.svg" alt=""></a></li>
-									<li><a href="<?php echo $fellow_options['mail'];?>"><img src="<?php echo get_template_directory_uri(); ?>/images/mail.svg" alt=""></a></li>
-								</ul>
-							</header>
-                              
+                    <div class="row">
+                        <?php if (have_posts()) : ?>
+                            <div class="col-lg-7">
+                                <header class="blog-header">
+                                    <div class="fellow-heading">
+                                        <h1 class="title"><?php the_archive_title() ?></h1>
+                                    </div>
+                                    <ul class="socials">
+                                        <li><a href="<?php echo $linkedin_url['url']; ?>" target="<?php echo $linkedin_url['target']; ?>"><?php echo get_inline_svg_social('linkedin-orange.svg'); ?></a></li>
+                                        <li><a href="<?php echo get_href_email($footer_email) ?>"><?php echo get_inline_svg('email-fill.svg'); ?></a></li>
+                                    </ul>
+                                </header>
                                 <?php
-                     
-                            /* Start the Loop */
-                            while ( have_posts() ) :
-                                the_post();?>
-
-                                    <article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
-                          
-                                        <h3 class="post-title"><a href="<?php echo get_permalink();?>"><?php the_title();?></a></h3>
-
-                                        <div class="post-content">
-                                            <?php
-                                                the_excerpt();
-                                            ?>
-                                        </div><!-- .post-content -->
-
-                                     
-                                        <div class="post-footer">
-                                            <div class="post-date"><?php echo get_the_date();?></div>
-                                            <div class="post-link"><a href="<?php echo get_permalink();?>" class="button simple arrow-right">Read All</a></div>
-                                        </div>
-                                    </article><!-- #post-<?php the_ID(); ?> -->
-                            <?php endwhile;
-
-                        wp_pagenavi(); 
-
-                        else :
-
-                            get_template_part( 'template-parts/content', 'none' );
-
-                        endif;
-                        ?>
-                   
+                                /* Start the Loop */
+                                while (have_posts()) :
+                                    the_post(); ?>
+                                    <?php echo get_template_part('template-parts/parts/news_item'); ?>
+                                <?php endwhile;
+                                wp_pagenavi(); ?>
+                            </div>
+                        <?php else : ?>
+                            <?php echo get_template_part('template-parts/content', 'none'); ?>
+                        <?php endif; ?>
+                        <div class="col-lg-5">
+                            <?php get_sidebar(); ?>
+                        </div>
+                    </div>
                 </div>
-
-                <?php get_sidebar();?>
             </div>
-                
         </div>
-	</main><!-- #main -->
-<?php
+    </div>
+</main><!-- #main -->
 
-get_footer();
+<?php get_footer();
