9b5801ea6844b5f525655ab9af79586e1cf65b82
Maksym Koitsan <maksym.k@codelibry.com>
2023-11-29 15:34:27 +0200

    fix testimonials slider

--- template-parts/parts/blockquote_item.php
+++ template-parts/parts/blockquote_item.php
@@ -1,6 +1,7 @@
 <?php
 $testimonial_item = $args['item'];
 $author_position = get_field('testimonials_position', $testimonial_item);
+$location = get_field('testimonials_location', $testimonial_item);
 ?>
 
 <div class="content-block text-color-gray mb-4 mb-sm-4">
@@ -13,10 +14,15 @@ $author_position = get_field('testimonials_position', $testimonial_item);
             <div class="testimonials__listItem__body">
             </div>
         </div>
-        <footer class="bg--white">
+        <footer class="bg--white <?php if ($location) {
+                                        echo 'two_line';
+                                    } ?>">
             <span class="text--size--32 text-color-black"><?php echo get_the_title($testimonial_item); ?></span>
             <?php if ($author_position) : ?>
                 <cite class="text--size--18 text--uppercase text-color-secondary font--normal"><?php echo $author_position; ?></cite>
+            <?php endif; ?>
+            <?php if ($location) : ?>
+                <cite class="location text--size--18 text-color-secondary text--uppercase"><?php echo $location; ?></cite>
             <?php endif; ?>
         </footer>
     </blockquote>
